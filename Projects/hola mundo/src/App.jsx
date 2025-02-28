14de73f08f9ee84c131e062a9445b55bb4378055
Jhon Cadavid <jairovalencia100k19@mail.com>
2025-02-28 10:07:04 -0500

    Tw Cards

--- Projects/hola mundo/src/App.jsx
+++ Projects/hola mundo/src/App.jsx
@@ -1,21 +1,19 @@
 import './App.css';
+import { TwitterFollowCard } from './TwitterFollowCard';
 export function App () {
+  
+
     return (
-        <article className='tw-followCard'>
-            <header>
-                <img src="https://unavatar.io/sindresorhus@gmail.com" alt="Avatar De Prueba" />
-                <div>
-                    <strong>Jhon Cadavid</strong>
-                    <span>@jhoncdeveloper</span>
-                </div>
-            </header>
-
-            <aside>
-                <button>
-                    Seguir
-                </button>
-            </aside>
-
-        </article>
+        <section className='App'>
+        
+        <TwitterFollowCard userName="midudev">
+        Miguel Angel Durán
+        </TwitterFollowCard>
+        
+        <TwitterFollowCard userName="jofpin">
+        José Pino
+        </TwitterFollowCard>
+       
+        </section>
     )
 }
